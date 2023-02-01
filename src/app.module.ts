402143f0a1169d68e783e9d729de380a0330991e
adityash1 <aditya167411@gmail.com>
2023-02-01 23:43:55 +0530

    service for blog apis

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BlogModule } from './blog/blog.module';
 
 const url = process.env.MONGO_URL;
 
 @Module({
-  imports: [MongooseModule.forRoot(url, { useNewUrlParser: true })],
+  imports: [MongooseModule.forRoot(url, { useNewUrlParser: true }), BlogModule],
   controllers: [AppController],
   providers: [AppService],
 })
