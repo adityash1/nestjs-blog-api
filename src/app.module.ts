bb888195152ecb8bb4880d09fa159da53dfbb461
adityash1 <aditya167411@gmail.com>
2023-02-02 15:10:30 +0530

    implemented auth using AuthO

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogModule } from './blog/blog.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 @Module({
   imports: [
     BlogModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://adityash1:2dbRL6ds50F8lEO8@cluster0.pkncnhq.mongodb.net/?retryWrites=true&w=majority',
-      { useNewUrlParser: true },
-    ),
+    MongooseModule.forRoot(process.env.MONGO_URL, { useNewUrlParser: true }),
   ],
   controllers: [AppController],
   providers: [AppService],
